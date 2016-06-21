b58126d8bca904a3a159d82f12ee646c6dee8a4d
StlMaris123 <stellamarisnjage@gmail.com>
2016-06-21 18:20:03 +0300

    instanciating variables

--- animal.rb
+++ animal.rb
@@ -10,12 +10,24 @@ class Bird
 end
 
 class Dog
+  def make_up_name
+    @name = "Bella"
+  end
+
   def talk
-    puts "bark"
+    puts "#{@name} barks"
   end
 
   def move(destination)
     puts "walking to #{destination}"
+  end
+
+  def make_up_age
+    @age = 10
+  end
+
+  def report_age
+    puts "#{@name} is #{@age}years old"
   end
 end
 #creating instances of the class
@@ -24,4 +36,7 @@ dog  = Dog.new
 
 #calling methods on the instances
 bird.move("trees")
+dog.make_up_name
 dog.talk
+dog.make_up_age
+dog.report_age
