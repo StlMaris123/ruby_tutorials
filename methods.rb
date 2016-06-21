ce385354ed8c71e23ac7b82e538396e73dd18c41
StlMaris123 <stellamarisnjage@gmail.com>
2016-06-21 11:41:48 +0300

    calling methods you have defined

---
+++ methods.rb
@@ -0,0 +1,11 @@
+def girl_child
+  puts "she has faced many challenges"
+  puts "boy is the strongest weakness"
+end
+
+def boy_child(height)
+  puts "girls love #{height} boys"
+end
+
+girl_child
+boy_child("tall")
