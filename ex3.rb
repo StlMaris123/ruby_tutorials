06bdc7a5aa615a4112a39ec62b559c6fb8551b11
StlMaris123 <stellamarisnjage@gmail.com>
2016-06-23 09:44:41 +0300

    exercise 3 on computations

---
+++ ex3.rb
@@ -0,0 +1,51 @@
+#mathematics
+#use of interpolation is how we insert Ruby computations
+#displays the following code
+puts "i will now count my hens"
+#executes the computations and interpolates it wit Hens and also Roosters
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100-25*3%4}"
+
+#displays the following code
+puts "I will now count my eggs"
+#Executes the following computation using PEMDA and displays the answer
+puts 3+2+1-5+4%2-1/4%6
+
+#displays the following code
+puts "is it true that 3 + 2 < 5 - 7"
+#compares the value of 3+2 and 5-7 and returns a boolean value
+puts 3 + 2 < 5 - 7
+
+#Executes the code inside the parenthesis and concatenates it with the string
+puts "what is 3 + 2? #{3+2}"
+puts "what is 5 - 7? #{5-7}"
+
+#Displays the following thhe code
+puts "oh thats why its false"
+puts "How about some more?"
+
+#executes the code inside the parenthesis and concatenates the boolean value value to the string
+puts "is it greater? #{5 > -2}"
+puts "is it greater or equal? #{5 >= -2}"
+puts "is it less or equal? #{5 <= -2}"
+
+#using floating point numbers
+puts "i will now count my hens"
+puts "Hens #{25.00 + 30.00 / 6.00}"
+puts "Roosters #{100.00 - 25.00 * 3.00 % 4.00}"
+
+puts "I will now count my eggs"
+puts 3.00 + 2.00 + 1.00 - 5.00 + 4.00 % 2.00 - 1.00 / 4.00 % 6.00
+
+puts "is it true that 3 + 2 < 5 - 7"
+puts 3 + 2 < 5 - 7
+
+puts "what is 3 + 2? #{3+2}"
+puts "what is 5 - 7? #{5-7}"
+
+puts "oh thats why its false"
+puts "How about some more?"
+
+puts "is it greater? #{5 > -2}"
+puts "is it greater or equal? #{5 >= -2}"
+puts "is it less or equal? #{5 <= -2}"
