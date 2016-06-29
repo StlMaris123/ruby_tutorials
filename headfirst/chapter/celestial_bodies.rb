ea537b87c39a6f52287c7d310a3076e55d0ee5a6
StlMaris123 <stellamarisnjage@gmail.com>
2016-06-29 10:36:38 +0300

    hash default blocks

--- headfirst/chapter/celestial_bodies.rb
+++ headfirst/chapter/celestial_bodies.rb
@@ -1,13 +1,17 @@
 class CelestialBody
-attr_accessor :type, :name
+  attr_accessor :type, :name
 end
-#setting up the default object
-default_body = CelestialBody.new
-default_body.type = 'planet'
-bodies = Hash.new(default_body)
-#inspect the default object
-p bodies.default
-#try to add value to the hash
-bodies['mars'].name = 'Mars'
-#inspect the object again
-p bodies.default
+bodies = Hash.new do |hash, key|
+  body = CelestialBody.new
+  body.type = "planet"
+  hash[key] = body
+  body 
+end
+bodies['Mars'].name = 'Mars'
+bodies['Europa'].name = 'Europa'
+bodies['Europa'].type = 'moon'
+bodies['Venus'].name = 'Venus'
+
+p bodies['Mars']
+p bodies['Europa']
+p bodies['Venus']
