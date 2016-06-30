0e2cd670ba9944f0d5085617bfa963bf5476a0af
StlMaris123 <stellamarisnjage@gmail.com>
2016-06-30 10:40:34 +0300

    capitalizing constants

--- headfirst/chapter10/steak.rb
+++ headfirst/chapter10/steak.rb
@@ -1,8 +1,8 @@
 class Steak
+  GRADE_SCORES = {"prime" => 3, "choice" => 2, "select" => 1}
   attr_accessor :grade
   def > (other)
-    grade_scores = {"prime" => 3, "choice" => 2, "select" => 1}
-    grade_scores[grade] > grade_scores[other.grade]
+    GRADE_SCORES[grade] > GRADE_SCORES[other.grade]
   end
 end
 first_steak = Steak.new
