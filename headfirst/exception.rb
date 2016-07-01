83e267fd72a045f71a3a77cf3dd28a3f9f689721
StlMaris123 <stellamarisnjage@gmail.com>
2016-07-01 09:28:07 +0300

    code when all is working properly

---
+++ headfirst/exception.rb
@@ -0,0 +1,33 @@
+class Oven
+  attr_accessor :contents
+  def turn_on
+    puts "turn on the oven"
+    @state = "on"
+  end
+  def turn_off
+    puts "turning off the oven"
+    @state = "off"
+  end
+
+  def bake
+    unless @state == "on"
+      raise "you need to turn on the oven first"
+    end
+    if @contents == nil
+      raise "There is nothing in the oven!"
+    end
+    "golden-brown #{contents}"
+  end
+end
+diner = ['chips', 'sausages', 'chicken']
+oven = Oven.new
+oven.turn_on
+diner.each do |item|
+  begin
+    oven.contents = item
+    puts "serving #{oven.bake}"
+  rescue => error
+    puts "error: #{error.message}"
+    end
+  end
+
