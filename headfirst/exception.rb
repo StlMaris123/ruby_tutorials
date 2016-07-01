cd55c86cc7a5da8f4ea31490078f1ed6ba920c1b
StlMaris123 <stellamarisnjage@gmail.com>
2016-07-01 10:17:31 +0300

    using custom exception classes

--- headfirst/exception.rb
+++ headfirst/exception.rb
@@ -1,3 +1,8 @@
+class OvenEmptyError < StandardError
+end
+class OvenOffError < StandardError
+end
+
 class Oven
   attr_accessor :contents
   def turn_on
@@ -11,23 +16,25 @@ class Oven
 
   def bake
     unless @state == "on"
-      raise "you need to turn on the oven first"
+      raise OvenOffError, "you need to turn on the oven first"
     end
     if @contents == nil
-      raise "There is nothing in the oven!"
+      raise OvenEmptyError, "There is nothing in the oven!"
     end
     "golden-brown #{contents}"
   end
 end
-diner = ['chips', "beef", 'chicken']
+diner = ['chips', nil, 'chicken']
 oven = Oven.new
 oven.turn_off
 diner.each do |item|
   begin
     oven.contents = item
     puts "serving #{oven.bake}"
-  rescue => error
+  rescue OvenEmptyError => error
     puts "error: #{error.message}"
+  rescue OvenOffError => error
+    oven.turn_on
   end
 end
 
