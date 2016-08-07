6af846be59dd4f5eb47a022720a48ee067d9a9f2
StlMaris123 <stellamarisnjage@gmail.com>
2016-08-07 11:15:36 +0300

    EX32, for_loop

---
+++ learn ruby the hard way/Ex32.rb
@@ -0,0 +1,28 @@
+the_count = [1,2,3,4,5,]
+fruits    = ['apples', 'oranges', 'pears', 'apicarots'] 
+change   = [1, 'pennies', 2, 'dimes', 3, 'quarters']
+
+#traditional style of for_loop
+for number in the_count
+  puts "this is  count #{number}"
+end
+
+
+fruits.each do |fruit|
+  puts "A fruits of type: #{fruit}"
+end
+
+#going through a mixed list
+change.each { |i| puts "I got #{i}" }
+
+#building lists
+elements = []
+
+#Range operator to do 0 to 5 counts
+(0..5).each do |i|
+  puts "adding #{i} to the list"
+  elements.push(i)
+end
+
+#printing the elements
+elements.each { |i| puts "Element was : #{i}" }
