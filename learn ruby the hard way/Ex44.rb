063d5130945327cbdd650e6aaee11a7281560690
StlMaris123 <stellamarisnjage@gmail.com>
2016-08-09 12:16:04 +0300

    difference between implicit inheritance and overriding

--- learn ruby the hard way/Ex44.rb
+++ learn ruby the hard way/Ex44.rb
@@ -1,10 +1,23 @@
 class Parent
+  def override()
+    puts "PARENT override"
+  end
+
+  def implicit()
+    puts "PARENT implicit()"
+  end
+
   def altered()
     puts "PARENT altered"
   end
 end
 
 class Child  < Parent
+  
+  def override()
+    puts "CHILD override()"
+  end
+
   def altered()
     puts "CHILD, BEFORE PARENET altered()"
     super()
@@ -14,6 +27,12 @@ class Child  < Parent
 end
 dad = Parent.new()
 son = Child.new()
+
+dad.implicit()
+son.implicit()
+
+dad.override()
+son.override()
 
 dad.altered()
 son.altered()
