3a5bc644b9f254003abd3bc36897b4c133a5b7ea
StlMaris123 <stellamarisnjage@gmail.com>
2016-08-09 11:38:29 +0300

    update class map

--- learn ruby the hard way/Ex43.rb
+++ learn ruby the hard way/Ex43.rb
@@ -203,20 +203,36 @@ class EcapePod < Scene
 	  
 	  return 'finished'
 	end
-
   end
+end
 
+class Finished < Scene
+  def enter()
+	puts "You won! Good job."
+  end
 end
 
 class Map
+  @@scenes = {
+	'central_corridor' => CentralCorridor.new(),
+	'laser_weapon_armory' => LaserWeaponArmory.new(),
+	'the_bridge' => TheBridge.new(),
+	'escape_pd' => EscapePod.new(),
+	'death' => Death.new(),
+	'finished' => Finished.new(),
+  }
 
   def initialize( start_scene)
+	@start_scene = start_scene
   end
 
   def next_scene( scene_name)
+	val = @@scenes[scene_scene]
+	return val
   end
 
   def opening_scene()
+	return next_scene(@start_scene)
   end
 
 end
