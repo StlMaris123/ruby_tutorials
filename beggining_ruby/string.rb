66b1ff2f77973a0c7d916b579a6172eae573b12a
StlMaris123 <stellamarisnjage@gmail.com>
2016-09-22 10:20:06 +0300

    string literals in multiple lines of code

---
+++ beggining_ruby/string.rb
@@ -0,0 +1,6 @@
+#span over multiple lines
+ x = %q{ I love this
+ whole thing
+ about coding.}
+ #{}can be replaced by <>, (), or !!
+ puts x
---
+++ learn ruby the hard way/grandma.rb
@@ -0,0 +1,8 @@
+puts "please talk:"
+input = gets.chomp
+loud_input = input.upcase
+if loud_input == input
+  puts "ilikuwa 1904"
+else
+  puts "sijakuget"
+end
