8b13a74b8c2149d5ea326c9322b335a40a3531e3
StlMaris123 <stellamarisnjage@gmail.com>
2016-10-24 19:32:56 +0300

    telegram bot

---
+++ ruby/bot.rb
@@ -0,0 +1,13 @@
+require 'telegram/bot'
+token = '298868137:AAFEXG1BsNZM_x3dbDcoxRCBYw9kDazYaUY'
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+    case message.text
+    when '/start'
+      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+    when '/stop'
+      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+    end
+  end
+end
+
