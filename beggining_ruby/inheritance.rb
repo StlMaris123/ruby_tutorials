e6d04d74fba781d18871ec6ac099d2546a909bac
StlMaris123 <stellamarisnjage@gmail.com>
2016-12-27 11:35:06 +0300

    inheritance

---
+++ beggining_ruby/inheritance.rb
@@ -0,0 +1,18 @@
+class ParentClass
+  def method1
+    puts "hello from method1 class"
+  end
+
+  def method2
+    puts "Hello from method2 class"
+  end
+end
+
+class ChildClass < ParentClass
+  def method2
+    puts "Hello from method2 in the child class"
+  end
+end
+
+my_object = ChildClass.new
+my_object.method1
