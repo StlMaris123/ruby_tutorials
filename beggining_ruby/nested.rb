c3c7eab839256abf3277a75aa5dc0b75b4994a9d
StlMaris123 <stellamarisnjage@gmail.com>
2016-12-31 13:39:08 +0300

    access cclasses nested within other classes

---
+++ beggining_ruby/nested.rb
@@ -0,0 +1,26 @@
+class Drawing
+  def self.give_me_a_circle
+    Circle.new
+  end
+
+  class Line
+    def my_line
+      "i am long and straight"
+    end
+  end
+
+  class Circle
+    def what_am_i
+      "This is a circle"
+    end
+  end
+end
+
+a = Drawing.give_me_a_circle
+puts a.what_am_i
+a = Drawing::Circle.new
+puts a.what_am_i
+a = Drawing::Line.new
+puts a.my_line
+a = Line.new
+puts a.my_line
