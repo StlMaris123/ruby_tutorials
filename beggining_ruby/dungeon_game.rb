6a84063505a270f18cc620cdb51830162b992331
StlMaris123 <stellamarisnjage@gmail.com>
2017-01-03 10:37:15 +0300

    create rooms

--- beggining_ruby/dungeon_game.rb
+++ beggining_ruby/dungeon_game.rb
@@ -5,7 +5,16 @@ class Dungeon
     @player = player
     @rooms = {}
   end
+
+  def add_room(reference, name, description, connections)
+    @rooms[reference] = Room.new(reference, name, description, connections)
+  end
 end
  
 Player = Struct.new(:name, :location)
-Room = Struct.new(:reference, :name, :description)
+Room = Struct.new(:reference, :name, :description, :connections)
+
+me = Player.new("Miss Njage")
+my_dungeon = Dungeon.new(me)
+my_dungeon.add_room(:largecave, "Large Cave", "a large caveronous cave", { :west => :smallcave })
+my_dungeon.add_room(:smallcave, "Small Cave", "a smmall, claustrophic cave", { :east => :largecave })
