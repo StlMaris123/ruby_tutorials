cf1068043579937571fcc08c1ba2e509927cbcd8
StlMaris123 <stellamarisnjage@gmail.com>
2017-01-03 10:21:22 +0300

    use strruct class to strore attributes

--- beggining_ruby/dungeon_game.rb
+++ beggining_ruby/dungeon_game.rb
@@ -6,23 +6,6 @@ class Dungeon
     @rooms = {}
   end
 end
-
-class Player
-  attr_accessor :name, :location
-
-  def initialize(player_name)
-    @name = player_name
-  end
-end
-
-class Room
-  attr_accessor :reference, :name, :description, :connections
-
-  def initialize(reference, name, description, connections)
-    @reference = reference
-    @name = name
-    @description = description
-    @connections = connections
-  end
-end
-
+ 
+Player = Struct.new(:name, :location)
+Room = Struct.new(:reference, :name, :description)
