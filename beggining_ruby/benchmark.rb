144b18da8b6f4d56fb69b9b9117cb58b1db94886
StlMaris123 <stellamarisnjage@gmail.com>
2017-01-23 12:03:52 +0300

    use benchmark to calculate code exe ution speed

---
+++ beggining_ruby/benchmark.rb
@@ -0,0 +1,16 @@
+require 'benchmark'
+iterations = 1000000
+
+b = Benchmark.measure do
+  for i in 1..iterations do
+    x = i
+  end
+end
+
+c = Benchmark.measure do
+  iterations.times do |i|
+    x = i
+  end
+end
+ puts b
+ puts c
