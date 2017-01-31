0436da414fef160f172fe2b2e7ca85ef48980a2d
StlMaris123 <stellamarisnjage@gmail.com>
2017-01-31 12:15:53 +0300

    update data in csv file

--- beggining_ruby/read.rb
+++ beggining_ruby/read.rb
@@ -7,3 +7,13 @@ p = CSV.parse(File.read('csv.txt'))
 puts p[0][0]
 puts p[1][0]
 puts p[2][0]
+
+person = CSV.read('csv.txt')
+laura = person.find { |p| p[0] =~/Laura/ }
+laura[0] = "Laura Smith"
+
+CSV.open('csv.txt', 'w') do |csv|
+  p.each do |p|
+    csv << person
+  end
+end
