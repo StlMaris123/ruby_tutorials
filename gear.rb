fec0f85d80381093bff93dffc6648caa8a4d179f
StlMaris123 <stellamarisnjage@gmail.com>
2017-10-25 10:49:15 +0300

    separate wheel and gear class each to handle a single resposnsibility

--- gear.rb
+++ gear.rb
@@ -1,9 +1,9 @@
 class Gear
   attr_reader :chainring, :cog, :wheel
-  def initialize(chainring, cog, rim, tire)
-    @chainring = chairing
+  def initialize(chainring, cog, wheel=nil)
+    @chainring = chainring
     @cog = cog
-    @wheel = Wheel.new(rim, tire)
+    @wheel = wheel
   end
 
   def ratio
@@ -13,12 +13,29 @@ class Gear
   def gear_inches
     ratio * wheel.diameter
   end
+end
 
-  Wheel = Struct.new(:rim, :tire) do
-    def diameter
-      rim + (tire * 2)
-    end
+class Wheel
+  attr_reader :rim, :tire
+
+  def initialize(rim, tire)
+    @rim = rim
+    @tire = tire
+  end
+
+  def diameter
+    rim + (tire * 2)
+  end
+
+  def circumference
+    diameter * Math::PI
   end
 end
 
-puts Gear.new(52, 11, 24, 1.25)
+wheel = Wheel.new(26, 1.5)
+puts wheel.circumference
+
+puts Gear.new(52, 11, wheel).gear_inches
+
+puts Gear.new(52, 11).ratio
+
