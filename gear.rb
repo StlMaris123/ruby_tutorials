83100efbf5a197a263cb31a67910ba5ddb30f97f
StlMaris123 <stellamarisnjage@gmail.com>
2017-11-03 09:10:44 +0300

    isolate mu;tiparameter initialization using modules

--- gear.rb
+++ gear.rb
@@ -1,54 +1,57 @@
-class Gear
-  attr_reader :chainring, :cog, :wheel
-  def initialize(args)
-    args = defaults.merge(args)
-    @chainring = args[:chainring]
-    @cog = args[:cog]
-    @wheel = args[:wheel]
-  end
+module SomeFramework
+  class Gear
+    attr_reader :chainring, :cog, :wheel
 
-  def ratio
-    chainring / cog.to_f
-  end
+    def initialize(chainring, cog, wheel)
+      @chainring = chainring
+      @cog = cog
+      @wheel = wheel
+    end
 
-  def gear_inches
-    ratio * wheel.diameter
-  end
+    def ratio
+      chainring / cog.to_f
+    end
 
-  def wheel
-    @wheel ||= Wheel.new(rim, tire)
-  end
+    def gear_inches
+      ratio * wheel.diameter
+    end
 
-  def defaults
-    {:chainring => 40, :cog => 18}
+    def wheel
+      @wheel ||= Wheel.new(rim, tire)
+    end
   end
 end
 
-class Wheel
-  attr_reader :rim, :tire
-
-  def initialize(rim, tire)
-    @rim = rim
-    @tire = tire
-  end
-
-  def diameter
-    rim + (tire * 2)
-  end
-
-  def circumference
-    diameter * Math::PI
+module GearWrapper
+  def self.gear(args)
+    SomeFramework::Gear.new(args[:chainring],
+                            args[:cog],
+                            args[:wheel])
   end
 end
 
-gear = Gear.new(
-                # :chairing => 52,
-                # :cog => 11,
-                :wheel => Wheel.new(26, 1.5)).gear_inches
-puts gear 
+  class Wheel
+    attr_reader :rim, :tire
 
-# puts Gear.new(52, 11, 20, 40).gear_inches
+    def initialize(rim, tire)
+      @rim = rim
+      @tire = tire
+    end
 
-# puts Gear.new(52, 11, 20, 40).ratio
+    def diameter
+      rim + (tire * 2)
+    end
+
+    def circumference
+      diameter * Math::PI
+    end
+  end
 
 
+
+   a  = GearWrapper.gear(
+    :chainring => 52,
+    :cog => 11,
+    :wheel => Wheel.new(26, 1.5)).gear_inches
+
+   puts a
