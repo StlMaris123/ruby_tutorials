52cb21fd6c92cb81debf10c1954b9806e32c1669
StlMaris123 <stellamarisnjage@gmail.com>
2018-03-20 23:19:47 +0300

    get and refactor input from the user

---
+++ well_grounded_rubyst/c2f.rb
@@ -0,0 +1,7 @@
+print "Hello. Please enter a Celsius value: "
+# celsius = gets
+# fahrenheit = (celsius.to_i * 9 / 5) + 32
+# print  "The Fahrenheit equivalent is is: "
+# print fahrenheit
+# puts "."
+print "The fahrenheit equivalent is ", gets.to_i * 9 / 5 + 32 , ".\n"
