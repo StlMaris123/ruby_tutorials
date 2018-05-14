412f3a29f8f26f64d61fcddd3b5c5c0dbf6f6af9
StlMaris123 <stellamarisnjage@gmail.com>
2018-05-14 13:58:56 +0300

    initial refactor, separate the methods

--- ruby_refactoring/customer.rb
+++ ruby_refactoring/customer.rb
@@ -12,33 +12,37 @@ class Customer
 
   def statement
     total_amount, frequent_renter_points = 0, 0
-    result = "RentalRecord for #{@name} \n"
-    @rental.each do |element|
-      this_amount = 0
+    result = "Rental Record for #{@name}\n"
+    @rentals.each do |element|
+      this_amount = amount_for(element)
 
-      case elemt.movie.price_code
-      when Movie::REGULAR
-        this_amount += 2
-        this_amount += (element.days_rented - 2) * 1.5 if elements.days_rented > 2
+      frequent_reneter_points += 1
 
-      when Movie::NEW_RELEASE
-        this_amount += elements.days_rented * 3
-
-      when Movie::CHILDRENS
-        this_amount += 1.5
-        this_amount += (element.days_reneted - 3) * 1.5 if element.days_rented > 3
-      end
-
-      frequent_renter_points += 1
-      if element.movie.price_code == Movie.NEW_RELEASE && element.days_reneted >
+      if element.movie.price_code == Movie.NEW_RELEASE && 
+        element.days_rented > 1
         frequent_renter_points += 1
       end
-      result += "\t" + element.movie.title + "\t" + this_amount.to_s + "\n"
-      total_amount += this_amount
-    end
 
-    result += "Amount is #{total_amount}\n"
+      result += "\t" + each.movie.title + "\t" + this_amount.to_s + "\n"
+      total_amount += this_amount
+
+    end
+    result += "Amount owed is #{total_amount}\n"
     result += "You earned #{frequent_renter_points} frequent renter points"
     result
+  end
+
+  def amount_for(element)
+    this_amount = 0
+    case element.movie.price_code
+    when Movie::REGULAR
+      this_amount += 2
+      this_amount += (element.days_rented - 2) * 1.5 if element.days_rented > 2
+    when Movie::NEW_RELEASE
+      this_amount += element.days_rented * 3
+    when Movie::CHILDRENS
+      this_amount += 1.5
+      this_amount += (element.days_rented - 3) * 1.5 if element.days_rented > 3
+    end
   end
 end
